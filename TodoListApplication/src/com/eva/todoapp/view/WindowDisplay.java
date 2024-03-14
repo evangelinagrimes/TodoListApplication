96c15d8328a3d6200ea522afa92dc7dd408c1bfa
Eva Grimes <evangelina.grimes.22@cnu.edu>
2024-03-14 15:03:59 -0400

    Added a button

--- TodoListApplication/src/com/eva/todoapp/view/WindowDisplay.java
+++ TodoListApplication/src/com/eva/todoapp/view/WindowDisplay.java
@@ -14,22 +14,32 @@ public class WindowDisplay extends JFrame{
 	
 	public WindowDisplay() {
 		
-		this.setSize(400,400);
+		this.setSize(800,600);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false); 
+		this.setTitle("Todo List");
 		
 		Toolkit tk = Toolkit.getDefaultToolkit();
-		
 		Dimension dim = tk.getScreenSize();
 		
-		int xPos = (dim.width/2) - (this.getWidth()/ 2);
-		int yPos = (dim.height/2) - (this.getHeight()/ 2);
-		
+		int xPos = (dim.width/2) - (this.getWidth()/2);
+		int yPos = (dim.height/2) - (this.getHeight()/2);
 		this.setLocation(xPos, yPos);
 		
-		this.setResizable(false); 
-
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// Panel object and content declaration
+		JPanel thePanel = new JPanel();
+		JLabel label1 = new JLabel("Insert Label here");
+		JButton addTaskButton = new JButton("Add Task");
 		
-		this.setTitle("Todo List");
+		label1.setText("New Text");
+		label1.setToolTipText("Doesn't do anything");
+		
+		// Updates the contents of the panel
+		thePanel.add(label1);
+		thePanel.add(addTaskButton);
+		
+		// Add the contents of the panel to the frame
+		this.add(thePanel); 
 		
 		this.setVisible(true);
 		
